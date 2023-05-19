d1aaf3cb9f7097938ed9c55594910f649c6fb056
Nathan Ireland <ngi@Nathans-MacBook-Pro.local>
2023-05-19 00:59:56 -0500

    first commit & sign in with firebase

---
+++ PocketGram/Controller/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.swift
+//  PocketGram
+//
+//  Created by Nathan Ireland on 5/18/23.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var signUpBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signUpAction(_ sender: Any) {
+        Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!)
+        
+        print("sign up")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
---
+++ PocketGram/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  PocketGram
+//
+//  Created by Nathan Ireland on 5/18/23.
+//
+
+import Foundation
