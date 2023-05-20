27769fe9a94693820ae5410213d062cf6bcf9425
Nathan Ireland <ngi@Nathans-MacBook-Pro.local>
2023-05-19 19:01:52 -0500

    Styled Login/Signin Screen

--- PocketGram/Controller/LoginViewController.swift
+++ PocketGram/Controller/LoginViewController.swift
@@ -21,6 +21,16 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
     @IBAction func loginAction(_ sender: Any) {
         Auth.auth().signIn(withEmail: emailField.text!, password: passwordField.text! ){authResult, error in
             
@@ -44,16 +54,6 @@ class LoginViewController: UIViewController {
         }
         
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
